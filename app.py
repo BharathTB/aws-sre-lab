2f3a3de22027758bb4571a1a46646cba5a1a9145
BharathTB <Bharathtb98@gmail.com>
2026-04-27 17:30:12 -0400

    Fix exclamation mark in status message

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 def status():
     node_name = socket.gethostname()
-    return f"<h1>Automation is fully working!!</h1><p>Running on: {node_name}</p>"
+    return f"<h1>Automation is fully working!!!</h1><p>Running on: {node_name}</p>"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
 INNER_EOF
