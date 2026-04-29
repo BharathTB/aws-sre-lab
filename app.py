37f1a88ab9e68db3125a6d5a4d70531b57667761
BharathTB <Bharathtb98@gmail.com>
2026-04-29 18:43:19 -0400

    Update status message in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def status():
     node_name = socket.gethostname()
-    return f"<h1>bharath is checking</h1><p>Running on: {node_name}</p>"
+    return f"<h1>bharath is checking again</h1><p>Running on: {node_name}</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
